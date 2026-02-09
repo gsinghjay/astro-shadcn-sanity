92e92dcb3412abca5cf70199f3b14a0dbcc29de4
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2026-02-09 00:57:24 -0500

    chore: move autoUpdates into deployment config and add appId for sanity deploy

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -16,5 +16,8 @@ export default defineCliConfig({
     dataset,
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to leanr more about using environment variables for local & production.
-  autoUpdates: true,
+  deployment: {
+    appId: 'zi1cig2o607y1js5cfoyird6',
+    autoUpdates: true,
+  },
 })
