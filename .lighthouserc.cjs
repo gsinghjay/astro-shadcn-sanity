b241e25a7d62907834f7e46ce9fa6a1a62725e7a
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2026-02-10 04:59:39 -0500

    feat: run Lighthouse CI in parallel with unit tests, limit to homepage
    Split ci.yml into parallel unit-tests and lighthouse jobs. Restrict
    LHCI to index.html and home/index.html instead of all discovered pages.

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -2,6 +2,10 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: './astro-app/dist',
+      url: [
+        'http://localhost/index.html',
+        'http://localhost/home/index.html',
+      ],
       numberOfRuns: 3,
     },
     upload: {
