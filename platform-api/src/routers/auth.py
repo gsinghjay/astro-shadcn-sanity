039374c3782a373220a9e1efca02b191372b0a7c
pac27-cloud <pac27@njit.edu>
2026-05-09 16:53:06 +0000

    changed auth endpoint format

--- platform-api/src/routers/auth.py
+++ platform-api/src/routers/auth.py
@@ -1,9 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, Form
 from models.settings import WorkerSettings
 from dependencies import get_settings, get_db
-
-import re
 
 router = APIRouter(prefix = "/auth", tags=["Authentication"])
 
@@ -11,17 +8,18 @@ CACHE_EXPIRY_SECONDS = 5 * 60 # 5 minute expiry time
 
 @router.post("/token", tags=["Authentication"])
 async def authenticate_for_access_token(
-    auth_data: OAuth2PasswordRequestForm = Depends(), 
+    username: str = Form(...),
+    password: str = Form(...),
     settings: WorkerSettings = Depends(get_settings), 
     db = Depends(get_db)
 ):
     if not settings.db or not settings.kv:
         raise HTTPException(500, "D1 or KV not configured")
     
-    cached_email = await settings.kv.get(f"session:{auth_data.password}")
+    cached_email = await settings.kv.get(f"session:{password}")
 
-    if cached_email and cached_email == auth_data.username:
-        return {"access_token": auth_data.password, "token_type": "bearer"}
+    if cached_email and cached_email == username:
+        return {"access_token": password, "token_type": "bearer"}
 
     query = """
         SELECT user.email 
@@ -29,15 +27,18 @@ async def authenticate_for_access_token(
         JOIN session ON user.id = session.user_id
         WHERE user.email = ? 
           AND session.token = ? 
-          AND user.role IN ('sponsor', 'admin') 
+          AND user.role IN ('sponsor') 
           AND session.expires_at > (unixepoch() * 1000);
     """
 
-    result = await db.prepare(query).bind(auth_data.username, auth_data.password).first()
+    try:
+        result = await db.prepare(query).bind(username, password).first()
+    except:
+        raise HTTPException(500, "Databse query failure")
     
     if not result:
         raise HTTPException(status_code=401, detail="Invalid session token or email")
         
-    await settings.kv.put(f"session:{auth_data.password}", result["email"], expirationTtl=CACHE_EXPIRY_SECONDS)
+    await settings.kv.put(f"session:{password}", result, expirationTtl=CACHE_EXPIRY_SECONDS)
 
-    return {"access_token": auth_data.password, "token_type": "bearer"} # swagger schema requirement
+    return {"access_token": password, "token_type": "bearer"} # swagger schema requirement
